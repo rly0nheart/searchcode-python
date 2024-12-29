d123f6e54e81b730309864731d3fc10d01ec4b85
rly0nheart <74001397+rly0nheart@users.noreply.github.com>
2024-12-29 12:09:06 +0200

    Minor fix

--- searchcode/__init__.py
+++ searchcode/__init__.py
@@ -2,5 +2,5 @@ from ._main import code_result, code_search, related_results
 
 __all__ = ["code_result", "code_search", "related_results"]
 __pkg__ = "searchcode"
-__pypi__ = f"https://pypi.org/project/{__package__}"
+__pypi__ = f"https://pypi.org/project/{__pkg__}"
 __version__ = "0.2.2"
