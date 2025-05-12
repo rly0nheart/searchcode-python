e537a9b68f9a230643147c5c77b0cd97009c403c
Ritchie Mwewa <74001397+rly0nheart@users.noreply.github.com>
2025-05-12 11:54:50 +0200

    Oops. Failed test

--- tests/test_searchcode.py
+++ tests/test_searchcode.py
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-from src.searchcode import Searchcode
+from searchcode import Searchcode
 
 sc = Searchcode(user_agent="Pytest")
 
